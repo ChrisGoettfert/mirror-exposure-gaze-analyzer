197acf4fba8e7a2c9d5783c178f8b3227c10e511
Christopher Göttfert <Christopher.Goettfert@.uni-wuerzburg.de>
2022-07-05 13:42:02 +0200

    refactored Fixation Calculator

--- Fixation_Calculator.py
+++ Fixation_Calculator.py
@@ -1,62 +1,55 @@
 import math
 import os
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import Scatterplot_on_Image
 
-input_features = 3
-filename = ""
-boundingboxes = None
 # List for eyes, chest, hips, feet
-AoI_Fixation_Lists = [[], [], [], []]
+aoi_fixation_lists = [[], [], [], []]
+average_errors = []
+fixations_during_timephases = []
+guided_dataframes = []
+exploration_dataframes = []
 
 
-def readInFixationData(filename):
+def read_in_fixation_data(fixations_filename):
     # read in data
-    df = pd.read_csv(filename)
+    df = pd.read_csv(fixations_filename)
 
     # drop all rows where fixation is not on surface
     df = df.loc[df['on_surf'] == True]
-    # Clean DF
+    # delete unneccessary columns from dataframe
     df = df[['fixation_id', 'norm_pos_x', 'norm_pos_y', 'duration', 'world_timestamp']]
-
-    #df.to_csv("fixations_on_surface_Surface 1_TRUE.csv")
-    # For each id in dataframe calculate the average of the norm_pos_x and norm_pos_y
+    # For each fixation id in dataframe calculate the average of the norm_pos_x and norm_pos_y as there are multiply entrys
     aggregation_functions = {'fixation_id': 'first', 'norm_pos_x': 'mean', 'norm_pos_y': 'mean', 'duration': 'mean',
                              'world_timestamp': 'mean'}
-    df_new = df.groupby(df['fixation_id']).aggregate(aggregation_functions)
+    df_grouped = df.groupby(df['fixation_id']).aggregate(aggregation_functions)
 
     # Calculate Timestamps of the fixations
-    df_new = calculateTimestamps(df_new)
+    df_grouped = calculate_timestamps(df_grouped)
+    x_positions = df_grouped['norm_pos_x']
+    y_positions = df_grouped['norm_pos_y']
 
-    # load the coordinates file
-    x = df_new['norm_pos_x']
-    y = df_new['norm_pos_y']
+    return df_grouped, x_positions, y_positions
 
-    return df_new, x, y
 
 # We need to calculate the time of the Fixation relevant to our video time so we can assign fixations to different tasks
-# e.g. Seconds 30-40 voice command was look at your eyes then we want those fixations and only those
+# e.g. Seconds 30-40 voice command was look at your eyes then we want only those fixations
 # Need to recalculate it, because the time in the results is in UNIX Time
-def calculateTimestamps(df):
+def calculate_timestamps(df):
     world_timestamps = pd.read_csv("Input\\world_timestamps.csv")
-    # Get the timestamp in seconds of the first frame recorded by our video from the world_timestamps csv (always exported)
-    # this is our reference to a point zero
+    # Get the timestamp in seconds of the first frame recorded by our video => point zero
     first_frame_time = world_timestamps['# timestamps [seconds]'][0]
     # to get the time value of our fixations we simply need to subtract the first_frame_time from our fixation times
     df["world_timestamp"] = df["world_timestamp"] - first_frame_time
     df = df.rename(columns={"world_timestamp": "time_in_video (seconds)", "duration": "duration (ms)"})
-    print()
-
     return df
 
 
-def calculateFixationsOnAoIs(boundingBoxes, df, AoI_Fixation_Lists):
-    print()
-    features = []
+def calculate_fixations_on_aois(bounding_boxes, df, aoi_fixation_lists):
+    areas_of_interest = []
     durations = []
 
     for datapoint in df.iterrows():
@@ -66,45 +59,44 @@ def calculateFixationsOnAoIs(boundingBoxes, df, AoI_Fixation_Lists):
         fixation_id = datapoint[0]
 
         datapoint_added = False
-        for bb in boundingBoxes:
-            if(bb[1].x0 <= x <= bb[1].x1 and bb[1].y0 <= y <= bb[1].y1):
+        for bb in bounding_boxes:
+            if (bb[1].x0 <= x <= bb[1].x1 and bb[1].y0 <= y <= bb[1].y1):
                 datapoint_added = True
-                if(bb[0] == "head"):
-                    AoI_Fixation_Lists[0].append(fixation_id)
-                    features.append(bb[0])
+                if (bb[0] == "head"):
+                    aoi_fixation_lists[0].append(fixation_id)
+                    areas_of_interest.append(bb[0])
                     durations.append(duration)
-                elif(bb[0] == "right_hand"):
-                    AoI_Fixation_Lists[1].append(fixation_id)
-                    features.append(bb[0])
+                elif (bb[0] == "right_hand"):
+                    aoi_fixation_lists[1].append(fixation_id)
+                    areas_of_interest.append(bb[0])
                     durations.append(duration)
                 elif (bb[0] == "left_hand"):
-                    AoI_Fixation_Lists[2].append(fixation_id)
-                    features.append(bb[0])
+                    aoi_fixation_lists[2].append(fixation_id)
+                    areas_of_interest.append(bb[0])
                     durations.append(duration)
-                elif(bb[0] == "feet"):
-                    AoI_Fixation_Lists[3].append(fixation_id)
-                    features.append(bb[0])
+                elif (bb[0] == "feet"):
+                    aoi_fixation_lists[3].append(fixation_id)
+                    areas_of_interest.append(bb[0])
                     durations.append(duration)
             # datapoint not in bounds of bb
         if not datapoint_added:
-            features.append(None)
+            areas_of_interest.append(None)
             durations.append(None)
 
-    df['AoI'] = features
+    df['AoI'] = areas_of_interest
     df['Durations'] = durations
-    Fixations_On_BoundingBox = df.pivot_table(index=['AoI'], aggfunc='size')
-    durations_in_AoIs = df.groupby(['AoI'])['Durations'].agg(lambda x: x.unique().sum())
-    print(durations_in_AoIs)
-    print (Fixations_On_BoundingBox)
-    results_list = [Fixations_On_BoundingBox, durations_in_AoIs]
+    fixations_on_bounding_box = df.pivot_table(index=['AoI'], aggfunc='size')
+    durations_in_aois = df.groupby(['AoI'])['Durations'].agg(lambda x: x.unique().sum())
+    print(durations_in_aois)
+    print(fixations_on_bounding_box)
+    results_list = [fixations_on_bounding_box, durations_in_aois]
     return results_list, df
 
 
-
-def saveResultsInLogFile(results_list, image_with_bb, average_errors, fixationsInTimePhases, dfExplo1, dfExplo2):
+def save_results_in_logfile(results_list, image_with_bb, average_errors, fixationsInTimePhases, df_explo1, df_explo2):
     # get path of result folder
     result_folder = os.path.dirname(image_with_bb)
-    dfexplo1res, dfexplo2res = explorationPhaseDataProcessing(dfExplo1, dfExplo2)
+    dfexplo1res, dfexplo2res = explorationPhaseDataProcessing(df_explo1, df_explo2)
     with open(result_folder + '\\Overallresults.txt', 'w') as f:
         for item in results_list:
             f.write("%s\n" % item)
@@ -125,16 +117,19 @@ def saveResultsInLogFile(results_list, image_with_bb, average_errors, fixationsI
         f.write("%s\n" % dfexplo2res)
     print(result_folder)
 
-def explorationPhaseDataProcessing(dfExplo1, dfExplo2):
-    dfExplo1 = dfExplo1.fillna("Not in any bb")
-    dfExplo1res = dfExplo1.pivot_table(index=['AoI'], aggfunc='size')
-    dfExplo2 = dfExplo2.fillna("Not in any bb")
-    dfExplo2res = dfExplo2.pivot_table(index=['AoI'], aggfunc='size')
+
+def explorationPhaseDataProcessing(df_explo1, df_explo2):
+    df_explo1 = df_explo1.fillna("Not in any bb")
+    dfExplo1res = df_explo1.pivot_table(index=['AoI'], aggfunc='size')
+    df_explo2 = df_explo2.fillna("Not in any bb")
+    dfExplo2res = df_explo2.pivot_table(index=['AoI'], aggfunc='size')
     print(dfExplo1res)
     return dfExplo1res, dfExplo2res
-def createHeatmapOnImage(x, y, feature_width, image, image_with_bb, VP_Index):
+
+
+def create_heatmap(x, y, image, image_with_bb, vp_ndex):
     # Construct 2D histogram from data using the 'plasma' colormap
-    plt.hist2d(x, y, bins=15, range= [[0, 1], [0,1]], density=True, cmap='plasma')
+    plt.hist2d(x, y, bins=15, range=[[0, 1], [0, 1]], density=True, cmap='plasma')
 
     # Plot a colorbar with label.
     cb = plt.colorbar()
@@ -145,29 +140,20 @@ def createHeatmapOnImage(x, y, feature_width, image, image_with_bb, VP_Index):
     plt.xlabel('x axis')
     plt.ylabel('y axis')
 
-    xcoords = [feature_width, feature_width * 2, feature_width * 3]
-    colors = ['r','r','r']
-
-    # Draw vertical line on image
-    #for xc,c in zip(xcoords,colors):
-        #plt.axvline(x=xc, label='line at x = {}'.format(xc), c=c)
-
-    # Show the plot.
-
     map_img = mpimg.imread(image)
     plt.imshow(map_img, zorder=0)
 
-    # Need to save before showing else its blank
     plt.savefig(os.path.join(os.path.dirname(image_with_bb),
-                              "Heatmap_VP" + str(VP_Index) + ".png"))
+                             "Heatmap_VP" + str(vp_ndex) + ".png"))
 
     plt.show()
 
-def calculateFixationsWithTimestamps(df, boundingboxes, phasename):
+
+def calculate_fixations_with_timestamps(df, bounding_boxes, phasename):
     bb = None
     counter = 0
     duration = 0
-    for b in boundingboxes:
+    for b in bounding_boxes:
         if (b[0] == phasename):
             bb = b
     for datapoint in df.iterrows():
@@ -177,14 +163,14 @@ def calculateFixationsWithTimestamps(df, boundingboxes, phasename):
             counter += 1
             duration += datapoint[1]["duration (ms)"]
 
-    duration = round((duration / 1000), 2)    # to seconds
+    duration = round((duration / 1000), 2)  # to seconds
     return [phasename, "Fixations Inside " + phasename + " " + str(counter), "Duration: " + str(duration) + " seconds"]
 
 
-def CalculateAverageErrorToBoundingBoxes(df, boundingBox, boundingBoxName):
+def calculate_average_error_to_bounding_boxes(df, boundingBox, boundingBoxName):
     bb = None
     for b in boundingBox:
-        if(b[0] == boundingBoxName):
+        if (b[0] == boundingBoxName):
             bb = b
     Mirror_Height = 112
     Mirror_Width = 59.3
@@ -192,83 +178,86 @@ def CalculateAverageErrorToBoundingBoxes(df, boundingBox, boundingBoxName):
     middlePointOfBoundingBoxYInCm = ((bb[1].y1 + bb[1].y0) / 2) * Mirror_Height
     results = []
     for datapoint in df.iterrows():
-
         distance = math.hypot(datapoint[1]["norm_pos_x"] * Mirror_Width - middlePointOfBoundingBoxXInCm,
                               datapoint[1]["norm_pos_y"] * Mirror_Height - middlePointOfBoundingBoxYInCm)
         results.append(distance)
-    res =  np.mean(results)
+    res = np.mean(results)
     return res
 
 
+def perform_timestamp_based_calculations(df, start_time, bounding_boxes):
+    df_all, df_explo1, df_head, df_right_hand, df_left_hand, df_feet, df_explo2 = split_dataframes_by_timestamps(df,
+                                                                                                                 start_time)
+
+    average_errors.append(["Head", calculate_average_error_to_bounding_boxes(df_head, bounding_boxes, "head")])
+    average_errors.append(
+        ["RightHand", calculate_average_error_to_bounding_boxes(df_right_hand, bounding_boxes, "right_hand")])
+    average_errors.append(
+        ["LeftHand", calculate_average_error_to_bounding_boxes(df_left_hand, bounding_boxes, "left_hand")])
+    average_errors.append(["Feet", calculate_average_error_to_bounding_boxes(df_feet, bounding_boxes, "feet")])
+
+    head_fixations = calculate_fixations_with_timestamps(df_head, bounding_boxes, "head")
+    right_hand_fixations = calculate_fixations_with_timestamps(df_right_hand, bounding_boxes, "right_hand")
+    left_hand_fixations = calculate_fixations_with_timestamps(df_left_hand, bounding_boxes, "left_hand")
+    feet_fixations = calculate_fixations_with_timestamps(df_feet, bounding_boxes, "feet")
+
+    fixations_during_timephases.extend(
+        [[head_fixations], [right_hand_fixations], [left_hand_fixations], [feet_fixations]])
+    guided_dataframes.extend([df_head, df_right_hand, df_left_hand, df_feet])
+    exploration_dataframes.extend([df_explo1, df_explo2])
 
 
-def startFixationCalculationOnBoundingBoxes(filename, boundingboxes, image, image_with_bb, VP_Index, start_time):
-    average_Errors = []
-    fixationsInTimePhases = []
+def create_scatterplots_on_image(df_all, image, image_with_bb, vp_index):
+    iterator = [[df_all, "All"], [guided_dataframes[0], "Head"], [guided_dataframes[1], "RightHand"], [
+        guided_dataframes[2], "LeftHand"],
+                [guided_dataframes[3], "Feet"], [exploration_dataframes[0], "Explorative1"], [exploration_dataframes[1],
+                                                                                              "Explorative2"]]
+
+    for i in iterator:
+        Scatterplot_on_Image.drawScatterplotOnImage(i[0], image, image_with_bb, vp_index, i[1])
+
+
+def start_fixation_calculation(fixations_filename, boundingboxes, image, image_with_bb, vp_index,
+                               start_time):
     image = "Input\\" + image
-    filename = "Input\\" + filename
-    print("FileName: " + filename)
-    print(os.path.dirname(os.path.realpath(__file__)))
-    df, x, y = readInFixationData(filename)
-    results_list, df  = calculateFixationsOnAoIs(boundingboxes, df, AoI_Fixation_Lists)
-    df_all, dfExplo1, dfHead, dfRightHand, dfLeftHand, dfFeet, dfExplo2 = SplitDataframesByTimeStamps(df, start_time)
-    average_Errors.append(["Head", CalculateAverageErrorToBoundingBoxes(dfHead, boundingboxes, "head")])
-    average_Errors.append(["RightHand", CalculateAverageErrorToBoundingBoxes(dfRightHand, boundingboxes, "right_hand")])
-    average_Errors.append(["LeftHand", CalculateAverageErrorToBoundingBoxes(dfLeftHand, boundingboxes, "left_hand")])
-    average_Errors.append(["Feet", CalculateAverageErrorToBoundingBoxes(dfFeet, boundingboxes, "feet")])
+    fixations_filename = "Input\\" + fixations_filename
+    print("Processing fixations in file: " + fixations_filename)
+
+    df, x_positions, y_positions = read_in_fixation_data(fixations_filename)
+    results_list, df = calculate_fixations_on_aois(boundingboxes, df, aoi_fixation_lists)
+
+    perform_timestamp_based_calculations(df, start_time, boundingboxes)
+    save_results_in_logfile(results_list, image_with_bb, average_errors, fixations_during_timephases,
+                            exploration_dataframes[0], exploration_dataframes[1])
+
+    create_heatmap(df['norm_pos_x'], df['norm_pos_y'], image, image_with_bb, vp_index)
+    create_scatterplots_on_image(df, image, image_with_bb, vp_index)
 
 
-
-    headFixations = calculateFixationsWithTimestamps(dfHead, boundingboxes, "head")
-    rightHandFixations = calculateFixationsWithTimestamps(dfRightHand, boundingboxes, "right_hand")
-    leftHandFixations = calculateFixationsWithTimestamps(dfLeftHand, boundingboxes, "left_hand")
-    feetFixations = calculateFixationsWithTimestamps(dfFeet, boundingboxes, "feet")
-
-    fixationsInTimePhases.extend([[headFixations], [rightHandFixations], [leftHandFixations], [feetFixations]])
-    saveResultsInLogFile(results_list, image_with_bb, average_Errors, fixationsInTimePhases, dfExplo1, dfExplo2)
-    createHeatmapOnImage(x, y, 0.33, image, image_with_bb, VP_Index)
-    Scatterplot_on_Image.drawScatterplotOnImage(df, image, image_with_bb, VP_Index, "All")
-    Scatterplot_on_Image.drawScatterplotOnImage(dfHead, image, image_with_bb, VP_Index, "Head")
-    Scatterplot_on_Image.drawScatterplotOnImage(dfRightHand, image, image_with_bb, VP_Index, "RightHand")
-    Scatterplot_on_Image.drawScatterplotOnImage(dfLeftHand, image, image_with_bb, VP_Index, "LeftHand")
-    Scatterplot_on_Image.drawScatterplotOnImage(dfFeet, image, image_with_bb, VP_Index, "Feet")
-    Scatterplot_on_Image.drawScatterplotOnImage(dfExplo1, image, image_with_bb, VP_Index, "Explo1")
-    Scatterplot_on_Image.drawScatterplotOnImage(dfExplo2, image, image_with_bb, VP_Index, "Explo2")
-
-def SplitDataframesByTimeStamps(df, start_time):
-    starttime = start_time
-    dfExplo1 = None
-    dfHead = None
-    dfRightHand = None
-    dfLeftHand = None
-    dfFeet = None
-                        # + 14 weil nach 14 gehts los mit der betrachtung, + 85 weil so lang ging die Betrachtung
-    mask = (df['time_in_video (seconds)'] > starttime + 14) & (df['time_in_video (seconds)'] <= starttime + 84)
+# Cut Dataframe by seconds based on the audioinstructions
+def split_dataframes_by_timestamps(df, start_time):
+    # Start at 14 since instruction was 14 seconds long. 84 = end of audio file
+    mask = (df['time_in_video (seconds)'] > start_time + 14) & (df['time_in_video (seconds)'] <= start_time + 84)
     df_all = df.loc[mask]
-    mask_Explo1 = (df['time_in_video (seconds)'] > starttime + 19) & (df['time_in_video (seconds)'] <= starttime + 41)
-    dfExplo1 = df.loc[mask_Explo1]
-    mask_Head = (df['time_in_video (seconds)'] > starttime + 44) & (df['time_in_video (seconds)'] <= starttime + 49)
-    dfHead = df.loc[mask_Head]
-    maskLeftHand = (df['time_in_video (seconds)'] > starttime + 52) & (df['time_in_video (seconds)'] <= starttime + 57)
-    dfLeftHand = df.loc[maskLeftHand]
-    maskRightHand = (df['time_in_video (seconds)'] > starttime + 60) & (df['time_in_video (seconds)'] <= starttime + 65)
-    dfRightHand = df.loc[maskRightHand]
-    maskFeet = (df['time_in_video (seconds)'] > starttime + 68) & (df['time_in_video (seconds)'] <= starttime + 73)
-    dfFeet = df.loc[maskFeet]
-    maskExplo2 = (df['time_in_video (seconds)'] > starttime + 76) & (df['time_in_video (seconds)'] <= starttime + 84)
-    dfExplo2 = df.loc[maskExplo2]
+    mask_explorative1 = (df['time_in_video (seconds)'] > start_time + 19) & (
+                df['time_in_video (seconds)'] <= start_time + 41)
+    df_explorative1 = df.loc[mask_explorative1]
+    mask_head = (df['time_in_video (seconds)'] > start_time + 44) & (df['time_in_video (seconds)'] <= start_time + 49)
+    df_head = df.loc[mask_head]
+    mask_left_hand = (df['time_in_video (seconds)'] > start_time + 52) & (
+                df['time_in_video (seconds)'] <= start_time + 57)
+    df_left_hand = df.loc[mask_left_hand]
+    mask_right_hand = (df['time_in_video (seconds)'] > start_time + 60) & (
+                df['time_in_video (seconds)'] <= start_time + 65)
+    df_right_hand = df.loc[mask_right_hand]
+    mask_feet = (df['time_in_video (seconds)'] > start_time + 68) & (df['time_in_video (seconds)'] <= start_time + 73)
+    df_feet = df.loc[mask_feet]
+    mask_explorative2 = (df['time_in_video (seconds)'] > start_time + 76) & (
+                df['time_in_video (seconds)'] <= start_time + 84)
+    df_explorative2 = df.loc[mask_explorative2]
 
-
-    return df_all, dfExplo1, dfHead, dfLeftHand, dfRightHand, dfFeet, dfExplo2
-
-
-
-
+    return df_all, df_explorative1, df_head, df_left_hand, df_right_hand, df_feet, df_explorative2
 
 
 if __name__ == '__main__':
     print()
-
-
-
-
